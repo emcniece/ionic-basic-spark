a012a94aafd058b6afb933aff3e1eb297a3fbc1b
Eric McNiece <emcniece@caorda.com>
2014-11-04 12:22:42 -0800

    linting

--- www/js/app.js
+++ www/js/app.js
@@ -1,17 +1,11 @@
 // Ionic Starter App
 
-// Angular JS localstorage - NPM independent
-// http://learn.ionicframework.com/formulas/localstorage/
-
-angular.module('ionic.utils', [])
-
-
-
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
+angular.module('ionic.utils', []);
 angular.module('ionicBasicSpark', ['ionic', 'starter.controllers', 'starter.services'])
 
 .run(function($ionicPlatform, $localStorage) {
